7ae9490d39856e5bde0fff08176aaffda3d67b49
Roman Zipp <ich@ich.wtf>
2024-04-20 12:09:32 +0200

    Add tanstack query

--- src/app/(start)/page.jsx
+++ src/app/(start)/page.jsx
@@ -1,6 +1,22 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+
 export default function Home() {
+    const query = {
+        _path: 'query',
+        query: '{host=~".+"}',
+    };
+
+    const { data, error } = useQuery({
+        queryKey: ['loki', query],
+        queryFn: async () => fetch(`/api/loki?${new URLSearchParams(query)}`, {
+            method: 'GET',
+        }).then((res) => res.json()),
+    });
+
+    console.log(data);
+
     return (
         <main>
             loki frontend
