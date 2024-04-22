a7ad58e0523419cec9e76009dd9857444502d5de
Roman Zipp <ich@ich.wtf>
2024-04-22 10:33:35 +0200

    ???????????????????

---
+++ src/app/(start)/components.jsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { Fragment, useMemo } from 'react';
+import Result from '@/components/Result';
+import useLabels from '@/hooks/useLabels';
+
+export default function Components() {
+    const { selectedLabels, filterValues } = useLabels();
+
+    const query = useMemo(() => {
+        const filters = selectedLabels.map(({ name, value }) => `${name}="${value}"`);
+
+        if (filters.length === 0) {
+            return null;
+        }
+
+        let since = null;
+
+        if (filterValues.start) {
+            since = {
+                'now-1h': '1h',
+                'now-3h': '3h',
+                'now-12h': '12h',
+                'now-1d': '1d',
+            }[filterValues.start];
+        }
+
+        return {
+            query: `{${filters.join(', ')}}`,
+            since,
+        };
+    }, [selectedLabels, filterValues]);
+
+    const { data: resultData } = useQuery({
+        queryKey: ['loki', query],
+        queryFn: async () => fetch(`/api/loki?${new URLSearchParams(query)}`, {
+            headers: {
+                'X-Loki-Path': 'query_range',
+            },
+        })
+            .then((res) => res.json())
+            .then((res) => res.data),
+        enabled: query !== null,
+    });
+
+    if (!query) {
+        return (
+            <div className="flex justify-center items-center min-h-[16rem]">
+                Select a label from the dropdowns above.
+            </div>
+        );
+    }
+
+    return (
+        <div className="p-4">
+            {resultData?.result?.length > 0 && resultData.result.map((result) => (
+                <Fragment key={JSON.stringify(result.stream)}>
+                    <Result result={result} />
+                </Fragment>
+            ))}
+        </div>
+    );
+}
