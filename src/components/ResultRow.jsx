62efb36f543e335de63f0cff3490386b047f61bd
Roman Zipp <ich@ich.wtf>
2024-07-14 07:47:25 +0200

    Syntax fixes

--- src/components/ResultRow.jsx
+++ src/components/ResultRow.jsx
@@ -63,7 +63,7 @@ function ResultRow({ row, truncate = false }) {
                 collapse
                 title={row.data.level}
             >
-                <span className={classNames(row.hasBackground ? '' : row.classNameMap.textClassName), 'px-2 py-[px]'}>
+                <span className={classNames(row.hasBackground ? '' : row.classNameMap.textClassName, 'px-2 py-[px]')}>
                     {row.data.level_name}
                 </span>
             </Td>
